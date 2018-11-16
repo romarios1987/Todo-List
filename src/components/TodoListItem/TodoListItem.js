0c3128eb23f1bf1ffecc86aec6e901dbd64e24f0
romarios1987 <romios047@gmail.com>
2018-11-16 22:55:39 +0200

    add event listener - onClick

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -3,13 +3,31 @@ import './TodoListItem.css'
 
 
 export default class TodoListItem extends Component {
+
+    // constructor() {
+    //     super();
+    //     this.onLabelClick = () => {
+    //         console.log(`Done: ${this.props.label}`)
+    //     };
+    // }
+
+    onLabelClick = () => {
+        console.log(`Done: ${this.props.label}`)
+    };
+
     render() {
 
         const {label, important = false} = this.props;
 
         const styleSpan = {
-            color: important ? 'red' : 'clack'
+            color: important ? 'red' : 'clack',
+            fontWeight: important ? '500' : 'normal'
         };
-        return <span className="TodoListItem" style={styleSpan}>{label}</span>
+        return (
+            <span className="TodoListItem" style={styleSpan}
+                  onClick={this.onLabelClick}>
+                {label}
+            </span>
+        )
     }
 }
