db3fb804959f201b5c359d43517158c139551b92
romarios1987 <romios047@gmail.com>
2019-02-28 19:18:08 +0300

    Updates

---
+++ precache-manifest.1c9fc3650d7e541cb9b32e2bd881f7ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f47f260b12c7ad9f72e",
+    "url": "/todo-list/static/js/runtime~main.6f47f260.js"
+  },
+  {
+    "revision": "a170d7e469dc94b6aec9",
+    "url": "/todo-list/static/js/main.a170d7e4.chunk.js"
+  },
+  {
+    "revision": "1d90114b20e71790f655",
+    "url": "/todo-list/static/js/1.1d90114b.chunk.js"
+  },
+  {
+    "revision": "a170d7e469dc94b6aec9",
+    "url": "/todo-list/static/css/main.6e9a2c60.chunk.css"
+  },
+  {
+    "revision": "673688e48b9f2f1a6c665d22ede2ebe6",
+    "url": "/todo-list/index.html"
+  }
+];
